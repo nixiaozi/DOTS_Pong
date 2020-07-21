925d10ef5386e605cef5fe98a938d2f3e9683c69
lee cheng <nixiaozi01@hotmail.com>
2020-07-22 01:11:04 +0800

    add Test Netcode  这个不能正常进行客户端连接

---
+++ DOTS_Pong/Assets/Pong/Scripts/Test/CubeInput.cs
@@ -0,0 +1,100 @@
+﻿
+
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
+using Unity.Transforms;
+using UnityEngine;
+
+
+// make sure you can send input from the client to the server.
+// This struct is responsible for serializing and deserializing the input data
+public struct CubeInput: ICommandData<CubeInput>
+{
+    public uint Tick => tick;
+    public uint tick;
+    public int horizontal;
+    public int vertical;
+
+
+    public void Deserialize(uint tick, ref DataStreamReader reader)
+    {
+        this.tick = tick;
+        horizontal = reader.ReadInt();
+        vertical = reader.ReadInt();
+    }
+
+    public void Serialize(ref DataStreamWriter writer)
+    {
+        writer.WriteInt(horizontal);
+        writer.WriteInt(vertical);
+    }
+
+    public void Deserialize(uint tick, ref DataStreamReader reader, CubeInput baseline,
+        NetworkCompressionModel compressionModel)
+    {
+        Deserialize(tick, ref reader);
+    }
+
+    public void Serialize(ref DataStreamWriter writer, CubeInput baseline, NetworkCompressionModel compressionModel)
+    {
+        Serialize(ref writer);
+    }
+
+
+}
+
+// The command stream consists of the current tick and the horizontal and vertical movements
+public class NetCubeSendCommandSystem : CommandSendSystem<CubeInput>
+{
+}
+public class NetCubeReceiveCommandSystem : CommandReceiveSystem<CubeInput>
+{
+}
+
+
+//To sample the input, send it over the wire. 对输入进行采样
+[UpdateInGroup(typeof(ClientSimulationSystemGroup))]
+public class SampleCubeInput : ComponentSystem
+{
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<NetworkIdComponent>();
+        RequireSingletonForUpdate<EnablePongGhostReceiveSystemComponent>();  // 这里引用了一个接受数据的系统 通过GhostCollection -- Generate collection code 生成对应的代码类
+    }
+
+    protected override void OnUpdate()
+    {
+        var localInput = GetSingleton<CommandTargetComponent>().targetEntity;
+        if (localInput == Entity.Null)
+        {
+            var localPlayerId = GetSingleton<NetworkIdComponent>().Value;
+            Entities.WithNone<CubeInput>().ForEach((Entity ent, ref MovableCubeComponent cube) =>
+            {
+                if (cube.PlayerId == localPlayerId)
+                {
+                    PostUpdateCommands.AddBuffer<CubeInput>(ent);
+                    PostUpdateCommands.SetComponent(GetSingletonEntity<CommandTargetComponent>(), new CommandTargetComponent { targetEntity = ent });
+                }
+            });
+            return;
+        }
+        var input = default(CubeInput);
+        input.tick = World.GetExistingSystem<ClientSimulationSystemGroup>().ServerTick;
+        if (Input.GetKey("a"))
+            input.horizontal -= 1;
+        if (Input.GetKey("d"))
+            input.horizontal += 1;
+        if (Input.GetKey("s"))
+            input.vertical -= 1;
+        if (Input.GetKey("w"))
+            input.vertical += 1;
+        var inputBuffer = EntityManager.GetBuffer<CubeInput>(localInput);
+        inputBuffer.AddCommandData(input);
+    }
+}
+
+
+
+
+
---
+++ DOTS_Pong/Assets/Pong/Scripts/Test/Game.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
+using Unity.Burst;
+
+
+//这个继承了ComponentSystem(ComponentSystemBase 是它的基类)
+//而普通的System使用的JobComponentSystem也继承了同样的基类ComponentSystemBase
+//所以它们具有同样的底层生命周期
+// Control system updating in the default world
+[UpdateInWorld(UpdateInWorld.TargetWorld.Default)]   // 指定操作的world空间
+public class Game : ComponentSystem
+{
+    // Singleton component to trigger connections once from a control system
+    //单例组件，可以从一个控制系统触发连接，因为它的访问控制符为internal
+    struct InitGameComponent : IComponentData
+    {
+    }
+
+    // 创建时执行，这样只会执行一次
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<InitGameComponent>(); // 确认InitGameComponent 是否是单例模式
+        // Create singleton, require singleton for update so system runs once
+        EntityManager.CreateEntity(typeof(InitGameComponent));  // 创建单例的实体
+    }
+
+    protected override void OnUpdate() // 每一帧更新时执行？
+    {
+        // Destroy singleton to prevent system from running again
+        EntityManager.DestroyEntity(GetSingletonEntity<InitGameComponent>());
+        foreach (var world in World.All)
+        {
+            var network = world.GetExistingSystem<NetworkStreamReceiveSystem>();
+            if (world.GetExistingSystem<ClientSimulationSystemGroup>() != null)
+            {
+                // Client worlds automatically connect to localhost
+                NetworkEndPoint ep = NetworkEndPoint.LoopbackIpv4; // 定义客户端连接的地址
+                ep.Port = 7979;
+                network.Connect(ep);
+            }
+#if UNITY_EDITOR
+            else if (world.GetExistingSystem<ServerSimulationSystemGroup>() != null)  // 注意外部添加了编辑器限定，只有在编辑器模式下才用server
+            {
+                // Server world automatically listens for connections from any host
+                NetworkEndPoint ep = NetworkEndPoint.AnyIpv4;  // 定义服务端程序侦听的地址
+                ep.Port = 7979;
+                network.Listen(ep);
+            }
+#endif
+        }
+    }
+}
+
+
+// client tell the server you are ready to start playing
+[BurstCompile]
+public struct GoInGameRequest : IRpcCommand
+{
+    public void Deserialize(ref DataStreamReader reader)
+    {
+    }
+
+    public void Serialize(ref DataStreamWriter writer)
+    {
+    }
+    [BurstCompile]
+    private static void InvokeExecute(ref RpcExecutor.Parameters parameters)
+    {
+        RpcExecutor.ExecuteCreateRequestComponent<GoInGameRequest>(ref parameters);
+    }
+
+    static PortableFunctionPointer<RpcExecutor.ExecuteDelegate> InvokeExecuteFunctionPointer =
+        new PortableFunctionPointer<RpcExecutor.ExecuteDelegate>(InvokeExecute);
+    public PortableFunctionPointer<RpcExecutor.ExecuteDelegate> CompileExecute()
+    {
+        return InvokeExecuteFunctionPointer;
+    }
+}
+
+
+// The system that makes the RPC request component transfer
+public class GoInGameRequestSystem : RpcCommandRequestSystem<GoInGameRequest>
+{
+}
+
+
+// When client has a connection with network id, go in game and tell server to also go in game
+[UpdateInGroup(typeof(ClientSimulationSystemGroup))]
+public class GoInGameClientSystem : ComponentSystem
+{
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<EnablePongGhostReceiveSystemComponent>(); // EnablePongGhostReceiveSystemComponent
+    }
+
+    protected override void OnUpdate()
+    {
+        Entities.WithNone<NetworkStreamInGame>().ForEach((Entity ent, ref NetworkIdComponent id) =>
+        {
+            PostUpdateCommands.AddComponent<NetworkStreamInGame>(ent);
+            var req = PostUpdateCommands.CreateEntity();
+            PostUpdateCommands.AddComponent<GoInGameRequest>(req);
+            PostUpdateCommands.AddComponent(req, new SendRpcCommandRequestComponent { TargetConnection = ent });
+        });
+    }
+}
+
+// When server receives go in game request, go in game and delete request
+[UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+public class GoInGameServerSystem : ComponentSystem
+{
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<EnablePongGhostReceiveSystemComponent>();  // EnablePongGhostReceiveSystemComponent
+    }
+
+    protected override void OnUpdate()
+    {
+        Entities.WithNone<SendRpcCommandRequestComponent>().ForEach((Entity reqEnt, ref GoInGameRequest req, ref ReceiveRpcCommandRequestComponent reqSrc) =>
+        {
+            PostUpdateCommands.AddComponent<NetworkStreamInGame>(reqSrc.SourceConnection);
+            UnityEngine.Debug.Log(String.Format("Server setting connection {0} to in game", EntityManager.GetComponentData<NetworkIdComponent>(reqSrc.SourceConnection).Value));
+#if true
+            var ghostCollection = GetSingleton<GhostPrefabCollectionComponent>();
+            var ghostId = PongGhostSerializerCollection.FindGhostType<CubeSnapshotData>();  // PongGhostSerializerCollection
+            var prefab = EntityManager.GetBuffer<GhostPrefabBuffer>(ghostCollection.serverPrefabs)[ghostId].Value;
+            var player = EntityManager.Instantiate(prefab);
+            EntityManager.SetComponentData(player, new MovableCubeComponent { PlayerId = EntityManager.GetComponentData<NetworkIdComponent>(reqSrc.SourceConnection).Value });
+
+            PostUpdateCommands.AddBuffer<CubeInput>(player);
+            PostUpdateCommands.SetComponent(reqSrc.SourceConnection, new CommandTargetComponent { targetEntity = player });
+#endif
+
+
+            PostUpdateCommands.DestroyEntity(reqEnt);
+        });
+    }
+}
