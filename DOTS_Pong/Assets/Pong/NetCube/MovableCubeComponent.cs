408d90ab2527304e50fe5223ac8b0af8f44bbe6f
lee cheng <nixiaozi01@hotmail.com>
2020-09-04 13:55:27 +0800

    fix network id error

--- DOTS_Pong/Assets/Pong/NetCube/MovableCubeComponent.cs
+++ DOTS_Pong/Assets/Pong/NetCube/MovableCubeComponent.cs
@@ -5,5 +5,6 @@ using Unity.NetCode;
 public struct MovableCubeComponent : IComponentData
 {
     // [GhostDefaultField(100,true)]  // 定义变量的量化 和是否使用插值
+    [GhostDefaultField] // 必须添加这个 才会在 Predicting player network id 下拉选择框中添加这个选项
     public int PlayerId;
 }
